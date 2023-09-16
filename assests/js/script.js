8913d677e06782032c11fcdc9dc26d3c876fe8be
Jimmy Wilson <james.l.wilson364@gmail.com>
2023-09-16 13:23:18 -0500

    saturday afternoon

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,17 +1,25 @@
 // get a handle on the variables
 var timerE1 = document.getElementById("timerQuestion");
-var scoreBoard = document.getElementById("#scoreboard");
-var startButton = document.getElementById("#start");
-var questionCurrent = document.getElementById("#questionFramework");
+var scoreBoard = document.getElementById("scoreboard");
+var startButton = document.getElementById("start");
+var questionOne = document.getElementById("questionOne");
+var startButton = document.getElementById("start");
+var startQuiz = document.getElementById("startQuiz");
+var confirmAnswer = document.getElementById("confirm");
+var numCorrect = 0;
+var numIncorrect = 0;
 
-// start button to begin the quiz
 
-// change content to first question
-// needs question
-// needs potential answers
-// needs to track the right answer
-// needs to capture the user input
-// needs a decision loop to determine if the answer if correct or incorrect
+var questionOneQues = document.createElement("div");
+var questionOneList = document.createElement("ol");
+var questionOneAnswerA = document.createElement("li");
+var questionOneAnswerABut = document.createElement("button");
+var questionOneAnswerB = document.createElement("li");
+var questionOneAnswerBBut = document.createElement("button");
+var questionOneAnswerC = document.createElement("li");
+var questionOneAnswerCBut = document.createElement("button");
+var questionOneAnswerD = document.createElement("li");
+var questionOneAnswerDBut = document.createElement("button");
 
 // needs to have a timer
 function countdown() {
@@ -28,7 +36,62 @@ function countdown() {
     }, 1000);
   }
 
-  countdown();  
+// start button to begin the quiz
+function callQuestionOne() {
+    questionOne.textContent = ("Question 1")
+    questionOneQues.textContent = ("The question");
+    questionOneAnswerABut.textContent = ("Answer A");
+    questionOneAnswerBBut.textContent = ("Answer A");
+    questionOneAnswerCBut.textContent = ("Answer A");
+    questionOneAnswerDBut.textContent = ("Answer A");
+    console.log(questionOneAnswerA);
+    console.log(questionOneAnswerB);
+    console.log(questionOneAnswerC);
+    console.log(questionOneAnswerD);
+    console.log(questionOneQues);
+    console.log(questionOne);
+    startButton.style.display = "none";
+    startQuiz.style.display = "none";
+    questionOne.appendChild(questionOneQues);
+    questionOneQues.appendChild(questionOneList);
+    questionOneList.appendChild(questionOneAnswerA);
+    questionOneAnswerA.appendChild(questionOneAnswerABut);
+    questionOneList.appendChild(questionOneAnswerB);
+    questionOneAnswerB.appendChild(questionOneAnswerBBut);
+    questionOneList.appendChild(questionOneAnswerC);
+    questionOneAnswerC.appendChild(questionOneAnswerCBut);
+    questionOneList.appendChild(questionOneAnswerD);
+    questionOneAnswerD.appendChild(questionOneAnswerDBut);
+    // timerE1.style.visibility = "visible";
+    countdown();
+    questionOneAnswerABut.addEventListener("click", function() {
+        confirmAnswer.textContent = "You are correct";
+    });
+    questionOneAnswerBBut.addEventListener("click", function() {
+        confirmAnswer.textContent = "You are incorrect";
+    });
+    questionOneAnswerCBut.addEventListener("click", function() {
+        confirmAnswer.textContent = "You are incorrect";
+    });
+    questionOneAnswerDBut.addEventListener("click", function() {
+        confirmAnswer.textContent = "You are incorrect";
+    });
+
+}
+    
+startButton.addEventListener("click", callQuestionOne);    
+
+
+// change content to first question
+// needs question
+// needs potential answers
+// needs to track the right answer
+// needs to capture the user input
+// needs a decision loop to determine if the answer if correct or incorrect
+
+
+
+ // countdown();  
 
 // needs to display number of correct answers out of total questions
 
